74a96150c44b122cbfcfbd8cef2171dccb899b4a
PeerPressured <Thore.klein@gmx.de>
2021-08-18 13:31:07 +0200

    Commit

---
+++ Main.py
@@ -0,0 +1,10 @@
+filename = 'Genome.txt'
+file = open(filename,'r')
+content = file.read().splitlines()
+file.close()
+genom = ''.join(content)
+basensequenz = input('Fügen Sie hier die Genomsequenz ein').lower()
+if basensequenz in genom:
+    print(True)
+else:
+    print(False)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Covid_Genome_Scanner
+Moin moin
