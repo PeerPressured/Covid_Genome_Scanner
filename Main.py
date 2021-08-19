a9579b9e862e4a46001d6eb7f8ad54aaf9d9dd41
PeerPressured <Thore.klein@gmx.de>
2021-08-19 18:10:56 +0200

    Commit

--- Main.py
+++ Main.py
@@ -1,10 +1,25 @@
-filename = 'Genome.txt'
+def sequenzenfinder(gen, seq):
+    copy = gen
+    displ = 0
+    indizes = []
+    while True:
+        if seq in copy:
+            index = copy.index(seq)
+            indizes.append(index+displ)
+            copy = copy[index+1:]
+            displ += index+1
+        else:
+            return indizes
+
+
+
+filename = 'LongCovid.txt'
 file = open(filename,'r')
 content = file.read().splitlines()
 file.close()
-genom = ''.join(content)
+genom = ''.join(content) #inhalt der text-datei beschönigt
 basensequenz = input('Fügen Sie hier die Genomsequenz ein').lower()
 if basensequenz in genom:
-    pass
+    print(sequenzenfinder(gen = genom, seq = basensequenz))
 else:
     print(False)
